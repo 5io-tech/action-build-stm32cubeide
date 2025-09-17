f4bfa164bb1a4554fbeb88a42c6140a9ff37c362
Dominik Wetzel <dominik.wetzel@5io.tech>
2025-09-17 13:44:19 +0200

    fix: ensure the ulimit is not over 9000

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# https://community.st.com/t5/stm32cubeide-mcus/cubeide-does-not-start-and-spawner-reaper/td-p/685814
+ulimit -n 8192
+
 stm32cubeide --launcher.suppressErrors -no-indexer -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /tmp/stm-workspace -importAll "$1"
 headless-build.sh -no-indexer -data /tmp/stm-workspace -build "$2"
